927c5ec4291a29f2e21122f3109c71a3f65b801a
Cyclingbob <bob21567@yahoo.com>
2025-09-20 22:55:12 +0100

    add clear display to test script

--- display_controller_test.py
+++ display_controller_test.py
@@ -8,6 +8,7 @@ low = 0
 high = 10000
 value = 5600
 
+display.clear_display()
 display.setNeedle(label_values, label_text, low, high, value)
 display.setLowerValue(value)
 display.setMiddleValue(value)

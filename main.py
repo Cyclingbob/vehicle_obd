fa967bff9fc84be1e80fb24660cb8534c5724bd1
Cyclingbob <bob21567@yahoo.com>
2025-09-21 15:02:16 +0100

    fix bugs and clean up

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from display import DisplayController, TextWidget, VirtualLED, CircularGauge
 from diagnostics import Vehicle
 from PIL import ImageFont
 
-from selected_metrics import getWatchedMetrics
-from metrics import Metric, our_metrics
-
 from getIPs import getIPs
 
 import copy
 import time
+import threading
 
 display = DisplayController(90)
 
@@ -16,11 +14,37 @@ fontAFile = "Inter_18pt-Medium.ttf"
 fontA = ImageFont.truetype(fontAFile, 14)
 
 vehicle = Vehicle() # Initialise Vehicle Object
-# success = vehicle.connect() # Connect to the vehicle
-success = False
 
-text_metrics = getWatchedMetrics() # get all user selected metrics to watch
+from webserver import run_webserver, watched_metrics_names, load_watched_metrics, get_watched_metrics_names, set_watched_metrics_names
+#import webserver
+from selected_metrics import getWatchedMetrics
+from metrics import Metric, our_metrics
+
+#text_metrics = getWatchedMetrics() # get all user selected metrics to watch
 watched_metrics = []
+
+def refresh_watched_metrics():
+    """
+    Populate the global watched_metrics list with Metric objects corresponding
+    to the currently selected watched_metrics_names (dictionary keys).
+    """
+    global watched_metrics
+    global watched_metrics_names
+
+    watched_metrics_names = get_watched_metrics_names()
+    watched_metrics = []
+
+    for metric_name in watched_metrics_names:
+        if metric_name in our_metrics:
+            got_metric = copy.deepcopy(our_metrics[metric_name])
+            got_metric.setVehicle(vehicle)
+            watched_metrics.append(got_metric)
+
+threading.Thread(target=run_webserver, daemon=True).start()
+refresh_watched_metrics()
+
+success = False
+active_status_led_toggle = False
 
 if not success: # If connection fails
     while not success: # while it is failing
@@ -30,6 +54,14 @@ if not success: # If connection fails
 
         info_text = TextWidget((5, 20), fontA, (255, 255, 255))
         elm_led = VirtualLED((5, 5), (255, 0, 0), 3)
+
+        active_status_led_toggle_led = VirtualLED((155, 5), (0, 0, 0), 3)
+        if active_status_led_toggle:
+            active_status_led_toggle_led.setColour((0, 0, 255))
+            active_status_led_toggle = False
+        else:
+            active_status_led_toggle = True
+        display.add_widget(active_status_led_toggle_led)
 
         if vehicle.ELM_connected:
             info_text.text = "ELM connected"
@@ -54,9 +86,14 @@ if not success: # If connection fails
 
         ips_render_y_count = ips_render_y_count + 10
 
-        for metric in text_metrics:
+#        for metric in text_metrics:
+
+        watched_metrics_names = get_watched_metrics_names()
+        refresh_watched_metrics()
+
+        for metric in watched_metrics:
             info_text = TextWidget((5, ips_render_y_count), fontA, (255, 255, 255))
-            info_text.text = metric + ": No car yet"
+            info_text.text = metric.getShort() + ": No car yet"
             ips_render_y_count = ips_render_y_count + 20
             display.add_widget(info_text)
 
@@ -65,23 +102,30 @@ if not success: # If connection fails
         time.sleep(1)
         success = vehicle.connect() # reconnect
 
-for metric in text_metrics:
-    got_metric = copy.deepcopy(our_metrics[metric]) # don't affect our_metrics in metrics.py
+for metric in watched_metrics:
+    got_metric = copy.deepcopy(our_metrics[metric.getKey()]) # don't affect our_metrics in metrics.py
     got_metric.setVehicle(vehicle) # So it knows where to send commands
     # got_metric.startWatching() # Subscribe
     watched_metrics.append(got_metric)
 
 vehicle.start()
 
+def getVehicle():
+    global vehicle
+    return vehicle
+
 while True:
+
+    watched_metrics_names = get_watched_metrics_names()
+    refresh_watched_metrics()
 
     display.clear_display()
     display.widgets = []
 
     # time.sleep()
-    value1 = watched_metrics[0].getValue()
-    value2 = watched_metrics[1].getValue()
-    value3 = watched_metrics[2].getValue()
+    #value1 = watched_metrics[0].getValue()
+    #value2 = watched_metrics[1].getValue()
+    #value3 = watched_metrics[2].getValue()
 
     # label_values = [0, 2500, 5000, 7500, 10000]
     # label_text = ["0", "2.5", "5", "7.5", "10"]
@@ -105,17 +149,12 @@ while True:
     display.add_widget(elm_led)
     display.add_widget(car_led)
 
-    metric1 = TextWidget((5, 20), fontA, (255, 255, 255))
-    metric1.text = f"{watched_metrics[0].getName()}: {value1} {watched_metrics[0].getUnit()}"
-
-    metric2 = TextWidget((5, 40), fontA, (255, 255, 255))
-    metric2.text = f"{watched_metrics[1].getName()}: {value2} {watched_metrics[1].getUnit()}"
-
-    metric3 = TextWidget((5, 60), fontA, (255, 255, 255))
-    metric3.text = f"{watched_metrics[2].getName()}: {value2} {watched_metrics[2].getUnit()}"
-
-    display.add_widget(metric1)
-    display.add_widget(metric2)
-    display.add_widget(metric3)
+    # Display watched metrics
+    for metric in watched_metrics:
+        value = metric.getValue() if vehicle.connected else "No car yet"
+        display.add_widget(TextWidget((5, y), fontA, (255, 255, 255)))
+        display.widgets[-1].text = f"{metric.getShort()}: {value} {metric.getUnit() if vehicle.connected else ''}"
+        y += 20
 
     display.refresh()
+    time.sleep(0.5)

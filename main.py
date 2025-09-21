d6b193bd0ea255a32afd1306011bd38dcbb7f12f
Cyclingbob <bob21567@yahoo.com>
2025-09-21 12:26:09 +0100

    Update to new display system and make it work better.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from display import DisplayController
+from display import DisplayController, TextWidget, VirtualLED, CircularGauge
 from diagnostics import Vehicle
+from PIL import ImageFont
 
 from selected_metrics import getWatchedMetrics
 from metrics import Metric, our_metrics
@@ -11,37 +12,58 @@ import time
 
 display = DisplayController()
 
+fontAFile = "Inter_18pt-Medium.ttf"
+fontA = ImageFont.truetype(fontAFile, 14)
+
 vehicle = Vehicle() # Initialise Vehicle Object
 # success = vehicle.connect() # Connect to the vehicle
 success = False
+
+text_metrics = getWatchedMetrics() # get all user selected metrics to watch
+watched_metrics = []
 
 if not success: # If connection fails
     while not success: # while it is failing
         
         display.clear_display()
+        display.widgets = []
+
+        info_text = TextWidget((5, 20), fontA, (255, 255, 255))
+        elm_led = VirtualLED((5, 5), (255, 0, 0), 3)
 
         if vehicle.ELM_connected:
-            display.warn(5, 20, "ELM connected")
-            display.drawVirtualLED(10, 10, (0, 255, 0), 3) # ELM status LED (connected)
+            info_text.text = "ELM connected"
+            elm_led.setColour((0, 255, 0)) # ELM status LED (connected)
         else:
-            display.warn(5, 20, "ELM disconnected")
-            display.drawVirtualLED(10, 10, (255, 0, 0), 3) # ELM status LED (disconnected)
-        
-        display.drawVirtualLED(20, 10, (255, 0, 0), 3) # vehicle status LED (disconnected)
+            info_text.text = "ELM disconnected"
+            elm_led.setColour((255, 0, 0)) # ELM status LED (disconnected)
+
+        display.add_widget(info_text)
+        display.add_widget(elm_led)
+
+        car_led = VirtualLED((15, 5), (255, 0, 0), 3) # vehicle status LED (disconnected)
+        display.add_widget(car_led)
 
         ips = getIPs()
-        ips_render_y_count = 30
+        ips_render_y_count = 40
         for ip in ips:
-            display.warn(5, ips_render_y_count, ip)
+            info_text = TextWidget((5, ips_render_y_count), fontA, (255, 255, 255))
+            info_text.text = ip
             ips_render_y_count = ips_render_y_count + 10
+            display.add_widget(info_text)
 
-        display.refresh(None)
+        ips_render_y_count = ips_render_y_count + 10
+
+        for metric in text_metrics:
+            info_text = TextWidget((5, ips_render_y_count), fontA, (255, 255, 255))
+            info_text.text = metric + ": No car yet"
+            ips_render_y_count = ips_render_y_count + 20
+            display.add_widget(info_text)
+
+        display.refresh()
 
         time.sleep(1)
         success = vehicle.connect() # reconnect
-
-text_metrics = getWatchedMetrics() # get all user selected metrics to watch
-watched_metrics = []
 
 for metric in text_metrics:
     got_metric = copy.deepcopy(our_metrics[metric]) # don't affect our_metrics in metrics.py
@@ -53,26 +75,47 @@ vehicle.start()
 
 while True:
 
+    display.clear_display()
+    display.widgets = []
+
     # time.sleep()
     value1 = watched_metrics[0].getValue()
     value2 = watched_metrics[1].getValue()
     value3 = watched_metrics[2].getValue()
 
-    label_values = [0, 2500, 5000, 7500, 10000]
-    label_text = ["0", "2.5", "5", "7.5", "10"]
-    low = 0
-    high = 10000
+    # label_values = [0, 2500, 5000, 7500, 10000]
+    # label_text = ["0", "2.5", "5", "7.5", "10"]
+    # low = 0
+    # high = 10000
 
-    display.setNeedle(label_values, label_text, low, high, value1)
-    display.setLowerValue(value2)
-    display.setMiddleValue(value3)
+    # display.setNeedle(label_values, label_text, low, high, value1)
+
+    elm_led = VirtualLED((5, 5), (255, 0, 0), 3)
+    car_led = VirtualLED((15, 5), (255, 0, 0), 3) # vehicle status LED (disconnected)
 
     if vehicle.ELM_connected:
-        display.drawVirtualLED(10, 10, (0, 255, 0), 3) # ELM status LED (connected)
+        elm_led.setColour((0, 255, 0)) # ELM status LED (connected) # ELM status LED (connected)
 
     if vehicle.connected:
-        display.drawVirtualLED(20, 10, (0, 255, 0), 3) # vehicle status LED (disconnected)
+        elm_led.setColour((0, 255, 0)) # ELM status LED (connected) # vehicle status LED (disconnected)
+        car_led.setColour((0, 255, 0))
     else:
-        display.drawVirtualLED(20, 10, (255, 0, 0), 3) # vehicle status LED (disconnected)
+        car_led.setColour((255, 0, 0))
 
-    display.refresh(value1)
+    display.add_widget(elm_led)
+    display.add_widget(car_led)
+
+    metric1 = TextWidget((5, 20), fontA, (255, 255, 255))
+    metric1.text = f"{watched_metrics[0].getName()}: {value1} {watched_metrics[0].getUnit()}"
+
+    metric2 = TextWidget((5, 40), fontA, (255, 255, 255))
+    metric2.text = f"{watched_metrics[1].getName()}: {value2} {watched_metrics[1].getUnit()}"
+
+    metric3 = TextWidget((5, 60), fontA, (255, 255, 255))
+    metric3.text = f"{watched_metrics[2].getName()}: {value2} {watched_metrics[2].getUnit()}"
+
+    display.add_widget(metric1)
+    display.add_widget(metric2)
+    display.add_widget(metric3)
+
+    display.refresh()

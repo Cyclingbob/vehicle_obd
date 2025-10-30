81d8355c2b8e841639a3d28d34e5fa5675003bcc
Cyclingbob <bob21567@yahoo.com>
2025-10-30 19:59:14 +0000

    remove the pointless if statement (if not success) add thread safety in refresh_watched_metrics

--- main.py
+++ main.py
@@ -31,14 +31,20 @@ def refresh_watched_metrics():
     global watched_metrics
     global watched_metrics_names
 
-    watched_metrics_names = get_watched_metrics_names()
+    metrics_lock = threading.Lock()
+    with metrics_lock:
+        watched_metrics_names = get_watched_metrics_names()
+
     watched_metrics = []
 
-    for metric_name in watched_metrics_names:
-        if metric_name in our_metrics:
-            got_metric = copy.deepcopy(our_metrics[metric_name])
-            got_metric.setVehicle(vehicle)
-            watched_metrics.append(got_metric)
+    if vehicle.connected:
+        for metric_name in watched_metrics_names:
+            if metric_name in our_metrics:
+                got_metric = copy.deepcopy(our_metrics[metric_name])
+                got_metric.setVehicle(vehicle)
+                watched_metrics.append(got_metric)
+    else:
+        print("Unable to refresh watched metrics as vehicle is not connected (vehicle.connected)")
 
 threading.Thread(target=run_webserver, daemon=True).start()
 refresh_watched_metrics()
@@ -46,67 +52,68 @@ refresh_watched_metrics()
 success = False
 active_status_led_toggle = False
 
-if not success: # If connection fails
-    while not success: # while it is failing
-        
-        display.clear_display()
-        display.widgets = []
+while not success: # while it is failing
+    
+    display.clear_display()
+    display.widgets = []
 
-        info_text = TextWidget((5, 20), fontA, (255, 255, 255))
-        elm_led = VirtualLED((5, 5), (255, 0, 0), 3)
+    info_text = TextWidget((5, 20), fontA, (255, 255, 255))
+    elm_led = VirtualLED((5, 5), (255, 0, 0), 3)
 
-        active_status_led_toggle_led = VirtualLED((155, 5), (0, 0, 0), 3)
-        if active_status_led_toggle:
-            active_status_led_toggle_led.setColour((0, 0, 255))
-            active_status_led_toggle = False
-        else:
-            active_status_led_toggle = True
-        display.add_widget(active_status_led_toggle_led)
+    active_status_led_toggle_led = VirtualLED((155, 5), (0, 0, 0), 3)
+    if active_status_led_toggle:
+        active_status_led_toggle_led.setColour((0, 0, 255))
+        active_status_led_toggle = False
+    else:
+        active_status_led_toggle = True
+    display.add_widget(active_status_led_toggle_led)
 
-        if vehicle.ELM_connected:
-            info_text.text = "ELM connected"
-            elm_led.setColour((0, 255, 0)) # ELM status LED (connected)
-        else:
-            info_text.text = "ELM disconnected"
-            elm_led.setColour((255, 0, 0)) # ELM status LED (disconnected)
+    if vehicle.ELM_connected:
+        info_text.text = "ELM connected"
+        elm_led.setColour((0, 255, 0)) # ELM status LED (connected)
+    else:
+        info_text.text = "ELM disconnected"
+        elm_led.setColour((255, 0, 0)) # ELM status LED (disconnected)
 
-        display.add_widget(info_text)
-        display.add_widget(elm_led)
+    display.add_widget(info_text)
+    display.add_widget(elm_led)
 
-        car_led = VirtualLED((15, 5), (255, 0, 0), 3) # vehicle status LED (disconnected)
-        display.add_widget(car_led)
+    car_led = VirtualLED((15, 5), (255, 0, 0), 3) # vehicle status LED (disconnected)
+    display.add_widget(car_led)
 
-        ips = getIPs()
-        ips_render_y_count = 40
-        for ip in ips:
-            info_text = TextWidget((5, ips_render_y_count), fontA, (255, 255, 255))
-            info_text.text = ip
-            ips_render_y_count = ips_render_y_count + 10
-            display.add_widget(info_text)
-
+    ips = getIPs()
+    ips_render_y_count = 40
+    for ip in ips:
+        info_text = TextWidget((5, ips_render_y_count), fontA, (255, 255, 255))
+        info_text.text = ip
         ips_render_y_count = ips_render_y_count + 10
+        display.add_widget(info_text)
+
+    ips_render_y_count = ips_render_y_count + 10
 
 #        for metric in text_metrics:
 
-        watched_metrics_names = get_watched_metrics_names()
-        refresh_watched_metrics()
+    watched_metrics_names = get_watched_metrics_names()
+    refresh_watched_metrics()
 
-        for metric in watched_metrics:
-            info_text = TextWidget((5, ips_render_y_count), fontA, (255, 255, 255))
-            info_text.text = metric.getShort() + ": No car yet"
-            ips_render_y_count = ips_render_y_count + 20
-            display.add_widget(info_text)
+    for metric in watched_metrics:
+        info_text = TextWidget((5, ips_render_y_count), fontA, (255, 255, 255))
+        info_text.text = metric.getShort() + ": No car yet"
+        ips_render_y_count = ips_render_y_count + 20
+        display.add_widget(info_text)
 
-        display.refresh()
+    display.refresh()
 
-        time.sleep(1)
-        success = vehicle.connect() # reconnect
+    time.sleep(1)
+    success = vehicle.connect() # reconnect
 
-for metric in watched_metrics:
-    got_metric = copy.deepcopy(our_metrics[metric.getKey()]) # don't affect our_metrics in metrics.py
-    got_metric.setVehicle(vehicle) # So it knows where to send commands
-    # got_metric.startWatching() # Subscribe
-    watched_metrics.append(got_metric)
+active_watched_metrics = []
+
+# for metric in watched_metrics:
+#     got_metric = copy.deepcopy(our_metrics[metric.getKey()]) # don't affect our_metrics in metrics.py
+#     got_metric.setVehicle(vehicle) # So it knows where to send commands
+#     # got_metric.startWatching() # Subscribe
+#     active_watched_metrics.append(got_metric)
 
 vehicle.start()
 
@@ -134,7 +141,10 @@ while True:
     display.add_widget(car_led)
 
     # Display watched metrics
-    for metric in watched_metrics:
+
+    y = 20
+
+    for metric in active_watched_metrics:
         value = metric.getValue() if vehicle.connected else "No car yet"
         display.add_widget(TextWidget((5, y), fontA, (255, 255, 255)))
         display.widgets[-1].text = f"{metric.getShort()}: {value} {metric.getUnit() if vehicle.connected else ''}"
